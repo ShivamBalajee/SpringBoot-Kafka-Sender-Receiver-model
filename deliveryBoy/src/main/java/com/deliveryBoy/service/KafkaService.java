2dd3e3ee168fb46f75d11e62b611463a53a27eec
unknown <shbala@iu.edu>
2024-02-12 10:09:36 -0500

    first commit

---
+++ deliveryBoy/src/main/java/com/deliveryBoy/service/KafkaService.java
@@ -0,0 +1,26 @@
+package com.deliveryBoy.service;
+
+import com.deliveryBoy.config.AppConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class KafkaService {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(KafkaService.class);
+
+    public boolean updateLocation(String location) {
+
+        this.kafkaTemplate.send(AppConstants.LOCATION_TOPIC_NAME, location);
+
+        return true;
+    }
+
+}
